81aee9568d23621b9c74633ee68b55b89cdec6a9
John Philip G. Cauilan <johnphilip.cauilan@cvsu.edu.ph>
2024-11-06 21:35:28 +0800

    NestJS-Assignment-3-Factorial-Calculator

---
+++ routes/assroutes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const { getFactorial } = require('../controller/asscontroller');
+
+
+router.get('/factorial/:number', (req, res) => {
+    const number = parseInt(req.params.number);
+    const result = getFactorial(number);
+
+    if (result.error) {
+        return res.status(400).json(result);
+    }
+
+    res.json(result);
+});
+
+module.exports = router;
