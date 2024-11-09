aa7b543da81f549a43ee9294c19ff33c2540558e
John Philip G. Cauilan <johnphilip.cauilan@cvsu.edu.ph>
2024-11-10 00:12:42 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
-// app.js
 const express = require('express');
 const app = express();
 const assignmentRoutes = require('./routes/ass1routes');
 
-// Use the assignment routes
+
 app.use('/api', assignmentRoutes);
 
-// Start the server
+
 const PORT = 9090;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
